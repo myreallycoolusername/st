80a7c181f82600c3fb64c8055d69fb2850ae8515
myreallycoolusername <91684986+myreallycoolusername@users.noreply.github.com>
2022-10-30 11:48:46 +0700

    Create uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/load/',
-    bare: '/bare/',
+    bare: '/b/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
